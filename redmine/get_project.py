d9699f8a0f12ab909f45e5edbab208874b5b9398
shkim8642 <102588649+shkim8642@users.noreply.github.com>
2022-05-31 15:11:03 +0900

    Update get_project.py

--- redmine/get_project.py
+++ redmine/get_project.py
@@ -1,9 +1,9 @@
 from redminelib import Redmine
 import requests
 
-redmine_url = "https://projects.rsupport.com"
-# redmine = Redmine(redmine_url, username="shkim", password="k135799!")
-redmine = Redmine('https://projects.rsupport.com/', key='1522ae601c99b45bc6739876fb923c1fcd82bf69')
+redmine_url = "레드마인URL"
+# redmine = Redmine(redmine_url, username="레드마인 ID", password="PASSWORD")
+redmine = Redmine('REDMINE URL', key='REDMINE_KEY')
 # print(list(redmine.project.all())) # list of project
 print("AAA")
 # print(len(redmine.project.all())) #number of project
